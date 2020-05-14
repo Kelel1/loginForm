c71d8f1e98750b4e8455c87a2f12552374eb6cbd
Kern Elder <kelel1@yahoo.com>
2020-05-14 02:36:47 -0400

    refractor code

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import LogIn from './LogIn';
+
+
+const App = () => {
+   
+    
+    return (
+      
+      <Container>
+        <LogIn></LogIn>
+      </Container>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LogIn from './components/LogIn';
-import Container from '@material-ui/core/Container'
+import App from './components/App';
 
-const App = () => {
-   
-    
-  return (
-    
-    <Container>
-      <LogIn></LogIn>
-    </Container>
-  )
-}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
