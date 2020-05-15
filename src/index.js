e62ceaa0cd0cad6b40e9e0e3571458d4215a112b
Kern Elder <kelel1@yahoo.com>
2020-05-14 23:18:26 -0400

    uninstall cors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App      from './components/App';
 import axios    from 'axios';
 
+
 axios.get('http://localhost:4000/api/login').then(response => {
   const login = response.data;
   console.log(login);
